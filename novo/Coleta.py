8c43dc09d12948c7f5db98cd58324ceff2bb55d1
JuliaOJustino <stephany.justino@sptech.school>
2023-11-30 21:27:29 -0300

    feat(): Coleta RAM

--- novo/Coleta.py
+++ novo/Coleta.py
@@ -14,7 +14,37 @@ def getPacotesEnviados():
 def getTaxaDeTransferencia():
     enviados = ps.net_io_counters(pernic=False, nowrap=True)[0]
     recebidos = ps.net_io_counters(pernic=False, nowrap=True)[1]
-    return conversaoMb((enviados+recebidos)/2)
+    total = conversaoMb((enviados + recebidos) / 2)
+    print(total)
+    return total
 
 
-getPacotesEnviados()
+def getPorcentagemUsoCpu():
+    per = ps.cpu_percent(interval=None)
+    print(per)
+    return per
+
+
+def getPorcentagemUsoRam():
+    mem = ps.virtual_memory()[2]
+    print(mem)
+    return mem
+
+
+def getUsoRam():
+    mem = ps.virtual_memory()[3]
+    print(mem)
+    return mem
+
+
+def getUsoTotal():
+    mem = ps.virtual_memory()[0]
+    print(mem)
+    return mem
+
+
+def getDisponivelRam():
+    mem = ps.virtual_memory()[1]
+    print(mem)
+    return mem
+
