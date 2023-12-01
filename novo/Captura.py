e6623a14094c0e08630bbedd0ec3c1a5aca208f8
JuliaOJustino <stephany.justino@sptech.school>
2023-12-01 00:20:57 -0300

    feat() Erro part 2

--- novo/Captura.py
+++ novo/Captura.py
@@ -9,10 +9,11 @@ cursor = conexao.conexao.cursor()
 
 
 def inserirRegistros(idServer, categoria, valor):
-    valor = int(valor)  # Convert string value to float
+    valor = int(valor)
+    print("Dados do insert: ", idServer, categoria, valor)
+    # Convert string value to float
     cursor.execute(
-        "INSERT INTO tb_registro VALUES (null, {idServer}, {categoria} NOW(), {valor});",
+        "INSERT INTO tb_registro VALUES (null, %d, %d, NOW(), %d);",
         (idServer, categoria, valor),
-
     )
     print("Registro inserido: ")
